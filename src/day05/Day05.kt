4763a93236aea91ffdeabb9705958afb80248cce
IThinkIGottaGo <llehnevaeh@vip.qq.com>
2021-12-05 14:02:24 +0800

    init day05

---
+++ src/day05/Day05.kt
@@ -0,0 +1,28 @@
+package day05
+
+import util.readInput
+
+/**
+ * --- 第 5 天：热泉历险 ---
+ *
+ *
+ */
+fun main() {
+    // 第一个问题
+    fun part1(input: List<String>): Int {
+        TODO()
+    }
+
+    // 第二个问题
+    fun part2(input: List<String>): Int {
+        TODO()
+    }
+
+    val testInput = readInput("day05_test")
+    check(part1(testInput) == 5)
+//    check(part2(testInput) == )
+
+    val input = readInput("day05")
+    println(part1(input))
+//    check(part2(input) == )
+}
